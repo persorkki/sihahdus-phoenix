133f59988d85af8ff5f9d32f885ca282aeb2311d
Persorkki <persorkki@protonmail.com>
2022-09-02 12:41:41 +0300

    some initial work

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+//import '../styles/globals.css'
+import Layout from '../components/Layout'
+import {
+  ChakraProvider,
+} from "@chakra-ui/react"
+import theme from '../themes/theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout>
+      <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
